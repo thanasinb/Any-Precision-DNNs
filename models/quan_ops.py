3f0288e23d2a3360c5e732ec6294c9ba74746dc1
thanasinb <thanasin.b@gmail.com>
2022-06-30 00:06:18 +0700

    [WIP] Log q size shape

--- models/quan_ops.py
+++ models/quan_ops.py
@@ -139,11 +139,11 @@ def mapMultiplierModel(q_x, q_w):
     logging.info('res.shape:')
     logging.info(res.shape)
 
-    logging.info('q_x.size(0)')
-    logging.info(q_x.size(0))
+    logging.info('q_x.size(0).shape')
+    logging.info(q_x.size(0).shape)
 
-    logging.info('q_w.size(1)')
-    logging.info(q_w.size(1))
+    logging.info('q_w.size(1).shape')
+    logging.info(q_w.size(1).shape)
 
     for h in range(q_x.size(0)):
         for i in range(q_x.size(1)):
