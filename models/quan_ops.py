14fb25cee01d19b4f40645749e8708ceb87c3d40
thanasinb <thanasin.b@gmail.com>
2022-06-30 01:11:49 +0700

    [WIP] Try mapping

--- models/quan_ops.py
+++ models/quan_ops.py
@@ -151,6 +151,7 @@ def mapMultiplierModel(q_x, q_w):
                 # logging.info('lut_diff[q_x[h, i, :], q_w_t[:, j]]')
                 # logging.info(lut_diff[index_select()])
                 logging.info(q_w[:, j])
+                logging.info(torch.index_select(q_w, 1, torch.tensor[j]))
                 # res[h][i][j] = torch.sum(lut_diff[q_x[h, i, :], q_w[:, j]])
                 res[h][i][j] = torch.sum(lut_diff[q_x[h, i, :], q_w[:, j]])
 
