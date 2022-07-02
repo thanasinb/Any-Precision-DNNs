3f4ab6e364b6cc8f088a630c5d18b55f69808bf3
thanasinb <thanasin.b@gmail.com>
2022-07-03 00:38:02 +0700

    [WIP] Log tensor.tolist()

--- models/quan_ops.py
+++ models/quan_ops.py
@@ -154,7 +154,7 @@ def mapMultiplierModel(q_x, q_w):
                 # logging.info(q_w)
                 logging.info(q_x[h, i, :].tolist())
                 logging.info(q_w[:, j].tolist())
-                logging.info(lut_diff[q_x[h, i, :].tolist(), q_w[:, j]].tolist())
+                logging.info(lut_diff[q_x[h, i, :].tolist(), q_w[:, j].tolist()])
                 # logging.info(torch.index_select(q_w, 1, torch.tensor[j]))
                 # res[h][i][j] = torch.sum(lut_diff[q_x[h, i, :], q_w[:, j]])
                 # res[h][i][j] = torch.sum(lut_diff[q_x[h, i, :], q_w[:, j]])
